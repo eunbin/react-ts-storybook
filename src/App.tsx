7fecce5446b55a5918438c61e02074a46f5aeead
Eunbeen Ahn <eunbeeny@kakao.com>
2020-05-20 23:53:16 +0900

    chore: add storybook

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+function App({ content }: { content: string }) {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          { content }
         </p>
         <a
           className="App-link"
